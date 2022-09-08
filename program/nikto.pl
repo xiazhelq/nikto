aeb12ed52a82ee55ee9104b32588e77b02d40ce7
xiazhelq <45841066+xiazhelq@users.noreply.github.com>
2022-09-08 17:23:26 +0800

    Update nikto.pl

--- program/nikto.pl
+++ program/nikto.pl
@@ -10,7 +10,6 @@ load_modules();
 #  Copyright (C) 2001 Chris Sullo
 #
 #  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; version 2
 #  of the License only.
 #
