e424c7d3779a677f3c619f8e83443a39bdb65c1b
xiazhelq <45841066+xiazhelq@users.noreply.github.com>
2022-09-08 17:27:39 +0800

    test
    test

--- program/nikto.pl
+++ program/nikto.pl
@@ -7,7 +7,6 @@ load_modules();
 ###############################################################################
 #                               Nikto                                         #
 ###############################################################################
-#  Copyright (C) 2001 Chris Sullo
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
